11579c2e2b721980d23a3c7318979a6138df5052
Karol Dzialowski <karlososhd@gmail.com>
2015-11-15 22:34:19 +0100

    Plan nauczycieli

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $backend->updateAllData();
 */
 
 // Przykladowe wyswietlanie planu dla klasy 4C
-$plan = R::getAll( 'SELECT * FROM plan WHERE klasa = 12' );
+$plan = R::getAll( 'SELECT * FROM plan WHERE klasa = 12 ORDER BY godzina ASC, dzien ASC' );
 $godziny = R::getAll( 'SELECT * FROM godziny' );
 $nauczyciele = R::getAll( 'SELECT * FROM nauczyciele' );
 $sale = R::getAll( 'SELECT * FROM sale' );
@@ -46,6 +46,61 @@ foreach ($plan as $lekcja) {
         if($nauczyciel_id != "") {
             $nauczyciel = R::getAll( "SELECT * FROM nauczyciele WHERE legacy_id = $nauczyciel_id" );
             echo '<a class="nauczyciel">'.$nauczyciel[0]['skrot'].'</a>';
+        }
+        
+        $sala_id = $lekcja['sala'];
+        if($sala_id != "") {
+            $sala = R::getAll( "SELECT * FROM sale WHERE legacy_id = $sala_id" );
+            $sala_skrot =  $sala[0]['skrot'];
+            
+            $sala_skrot = substr($sala_skrot, 0, 2);
+            echo '<a class="sala">'.$sala_skrot."</a>";
+        }
+        //$lekcja['nauczyciel'];
+        //echo $nauczyciele[(int)$lekcja['nauczyciel']]['skrot'];
+    echo '</td>';
+    
+    if($lekcja['dzien'] == '5') {
+        echo '</tr>';
+    }
+}
+
+// Przykladowe wyswietlanie planu dla nauczyciela 19
+$plan = R::getAll( 'SELECT * FROM plan WHERE nauczyciel = 6 ORDER BY godzina ASC, dzien ASC' );
+$godziny = R::getAll( 'SELECT * FROM godziny' );
+$nauczyciele = R::getAll( 'SELECT * FROM nauczyciele' );
+$sale = R::getAll( 'SELECT * FROM sale' );
+
+echo '<table>';
+echo "<thead>";
+echo "<tr>";
+echo "<th>godzina</th>";
+echo "<th>Poniedziałek</th>";
+echo "<th>Wtorek</th>";
+echo "<th>Środa</th>";
+echo "<th>Czwartek</th>";
+echo "<th>Piątek</th>";
+echo "</tr>";
+echo "</thead>";
+
+$hour_index = 0;   
+foreach ($plan as $lekcja) {
+
+    if($lekcja['dzien'] == '1') {
+        echo '<tr>';
+        echo '<td class="hour">';
+        $hour = $godziny[$hour_index];
+        echo $hour['start']."-".$hour['stop'];
+        echo '</td>';
+        $hour_index += 1;
+    }
+    
+    echo '<td ckass="lesson">';
+        echo $lekcja['przedmiot'];
+        
+        $klasa_id = $lekcja['klasa'];
+        if($klasa_id != "") {
+            echo '<a class="nauczyciel">'.$klasa.'</a>';
         }
         
         $sala_id = $lekcja['sala'];

533745c46ba96f4377254b1d158f78278bc4664d
Karol Dzialowski <karlososhd@gmail.com>
2015-11-26 20:45:54 +0100

    Przygotowanie do wgrania slimphp. Wstepne api do json.

--- ScheduleController.php
+++ ScheduleController.php
@@ -4,6 +4,7 @@ require_once 'db_connect.php';
 
 /**
  * Class for generating schedule responses.
+ * TODO: generowanie planow do json
  */
 class ScheduleController
 {
@@ -220,4 +221,55 @@ class ScheduleController
             echo "<a href='?type=classroom&id=".$classroom['legacy_id']."'>".$classroom['skrot'].'</a><br>';
         }
     }
+
+    public function getClassScheduleJson() {
+
+    }
 }
+
+// load required files
+require 'Slim/Slim.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+\Slim\Slim::registerAutoloader();
+R::freeze(true);
+$app = new \Slim\Slim();
+$app->get('/plan', function () use ($app) {
+  $articles = R::find('plan');
+  $app->response()->header('Content-Type', 'application/json');
+  echo json_encode(R::exportAll($articles));
+});
+class ResourceNotFoundException extends Exception {}
+$app->get('/plan/:id', function ($id) use ($app) {
+  try {
+      $plan = R::find('plan', 'klasa = ?', array($id));
+    if ($plan) {
+      $app->response()->header('Content-Type', 'application/json');
+      echo json_encode(R::exportAll($plan));
+    } else {
+      throw new ResourceNotFoundException();
+    }
+  } catch (ResourceNotFoundException $e) {
+    $app->response()->status(404);
+  } catch (Exception $e) {
+    $app->response()->status(400);
+    $app->response()->header('X-Status-Reason', $e->getMessage());
+  }
+});
+$app->get('/plan/:id/:day', function ($id, $day) use ($app) {
+  try {
+      $plan = R::find('plan', "klasa = $id AND dzien = $day", array($id));
+    if ($plan) {
+      $app->response()->header('Content-Type', 'application/json');
+      echo json_encode(R::exportAll($plan));
+    } else {
+      throw new ResourceNotFoundException();
+    }
+  } catch (ResourceNotFoundException $e) {
+    $app->response()->status(404);
+  } catch (Exception $e) {
+    $app->response()->status(400);
+    $app->response()->header('X-Status-Reason', $e->getMessage());
+  }
+});
+$app->run();

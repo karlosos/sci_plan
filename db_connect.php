afab0c6affb1cf75da8350011c545607aa888574
Karol Dzialowski <karlososhd@gmail.com>
2015-11-28 22:20:22 +0100

    Revert "Przygotowanie do wgrania slimphp. Wstepne api do json."
    This reverts commit 533745c46ba96f4377254b1d158f78278bc4664d.

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,8 @@
 <?php
 
-// TODO: tworzenie tabel przy pierwszym wlaczeniu
 require 'rb.php';
 R::setup('mysql:host=localhost;dbname=plan', 'root', 'osiem');
 ini_set('max_execution_time', 1800); //300 seconds = 5 minutes
+
+
+
